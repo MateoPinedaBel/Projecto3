57f59f8d256d13f6566f15deed1bcb08046129aa
MateoPinedaBel <MateoPinedaBel@Outlook.com>
2025-05-27 11:02:06 -0500

    feat creación de repositorio.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+public class Main {
+    public static void main(String[] args) {
+        String Nombre1 = "Mateo Andrés Pineda Beltrán";
+        String Nombre2 = "Edwin Smith Murillo Murillo";
+        String Nombre3 = "Geraldinne Jaramillo Avendaño";
+        String Nombre4 = "Dilan Esteban Villada Mejía";
+        String Nombre5 = "Dariannys Daniela Zambrano Escobar";
+        String Nombre6 = "Justin Londoño Cárdenas";
+        String Nombre7 = "Mariana Marín Vanegas";
+        int edad1 = 19;
+        int edad2 = 20;
+        int edad3 = 21;
+        int edad4 = 22;
+        int edad5 = 17;
+        int edad6 = 18;
+        int edad7 = 23;
+        int cedula1 = 1066869266;
+        int cedula2 = 1111668224;
+        int cedula3 = 1000443480;
+        int cedula4 = 1193565987;
+        int cedula5 = 1127923098;
+        int cedula6 = 1020416721;
+        int cedula7 = 1000440959;
+        }
+    }
